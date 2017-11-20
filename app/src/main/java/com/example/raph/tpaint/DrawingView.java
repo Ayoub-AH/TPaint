77e7af9efa889646fc50706284261396ef87f69a
Ayoub-AH <aub.al.h@gmail.com>
2017-11-20 11:12:23 -0500

    les activitees

---
+++ app/src/main/java/com/example/raph/tpaint/DrawingView.java
@@ -0,0 +1,16 @@
+package com.example.raph.tpaint;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Job on 20/11/2017.
+ */
+
+public class DrawingView extends View {
+    public DrawingView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/example/raph/tpaint/ModelDrawingActivity.java
@@ -0,0 +1,13 @@
+package com.example.raph.tpaint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModelDrawingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_model_drawing);
+    }
+}
