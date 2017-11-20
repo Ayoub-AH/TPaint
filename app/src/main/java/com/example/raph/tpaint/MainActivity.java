8c8d3614369deb350388ebf46f38e8eb8eb3c5c7
Raph <raphael.fustes@gmail.com>
2017-11-20 11:36:34 -0500

    Link button to activity + theme

--- app/src/main/java/com/example/raph/tpaint/MainActivity.java
+++ app/src/main/java/com/example/raph/tpaint/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.raph.tpaint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         b1 = findViewById(R.id.button);
-        b2 = findViewById(R.id.button3);
+        b2 = findViewById(R.id.button2);
+        b3 = findViewById(R.id.button3);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), FreeDrawingActivity.class);
+                startActivity(i);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), ModelDrawingActivity.class);
+                startActivity(i);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+
     }
+
 }
